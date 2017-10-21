643f4d0a2e384440de86e2c4f73985c6dcc2a793
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2017-10-21 23:53:45 +0800

    menampilkan inisialisasi luas rumah

---
+++ praktek0204.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double lr,p=1.87596,jbp,bp,bpp,a,b,jbph,tb;
+	cout<<"Luas rumah : ";cin>>lr;
+	cout<<"Jumlah box parket :";cin>>jbp;
+	cout<<""<<endl;
+	cout<<""<<endl;
+	
+}
